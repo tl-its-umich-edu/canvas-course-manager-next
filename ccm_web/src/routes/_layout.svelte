356d9c1b54711890fbf6e397faf7a6bf961bf8fd
Sam Sciolla <ssciolla@umich.edu>
2021-02-09 16:49:07 -0500

    Add missing newlines

--- ccm_web/src/routes/_layout.svelte
+++ ccm_web/src/routes/_layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import Nav from '../components/Nav.svelte';
+	import Nav from '../components/Nav.svelte'
 
-	export let segment: string;
+	export let segment: string
 </script>
 
 <style>
