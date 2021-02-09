3597d2ee6f89366b965eb71b632b0169f170e69a
Sam Sciolla <ssciolla@umich.edu>
2021-02-09 16:32:00 -0500

    Replace polka with express

--- ccm_web/src/server.ts
+++ ccm_web/src/server.ts
@@ -1,17 +1,18 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from 'sirv'
+import compression from 'compression'
+import express from 'express'
 
-const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+import * as sapper from '@sapper/server'
 
-polka() // You can also use Express
-	.use(
+const { PORT, NODE_ENV } = process.env
+const dev = NODE_ENV === 'development'
+
+const app = express()
+
+app.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+	.listen(PORT)
+	.on('error', (err) => console.log('error', err))
