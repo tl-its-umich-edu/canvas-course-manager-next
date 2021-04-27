c337278bc310a1626970b942a0754343f899faa8
Sam Sciolla <ssciolla@umich.edu>
2021-04-27 12:51:50 -0400

    Merge changes from master
    Move api/hello and api/globals into Nest app
    
    Integrate config and logging approaches with Nest
    
    Delete apiRouter and appHandler
    
    Switch to useStaticAssets; add WIP lti module
    
    Change setUpLTI to LTIService; handle Promise in constructor of LTIMidddleware
    
    Fix tests for AppController
    
    Move AppService, AppController into BaseModule
    
    Add an auth fail case end-to-end test; specify detectOpenHandles
    
    Rename Base* to API* for APIModule

---
+++ ccm_web/server/db/migrations/20210513112551-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      lastName: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      loginId: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
