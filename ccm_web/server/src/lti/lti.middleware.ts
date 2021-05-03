8062bca33f34fff8959b4aa01290be13284e43aa
Sam Sciolla <ssciolla@umich.edu>
2021-05-03 17:22:52 -0400

    Change setUpLTI to LTIService; handle Promise in constructor of LTIMidddleware

--- ccm_web/server/src/lti/lti.middleware.ts
+++ ccm_web/server/src/lti/lti.middleware.ts
@@ -1,32 +1,29 @@
-import { Injectable, NestMiddleware } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { Express, Request, Response, NextFunction } from 'express'
+import { Injectable, NestMiddleware } from '@nestjs/common'
 
-import { DatabaseConfig, LTIConfig } from '../config'
-import setUpLTI from './lti.setup'
+import { LTIService } from './lti.service'
+
+import baseLogger from '../logger'
+
+const logger = baseLogger.child({ filePath: __filename })
 
 @Injectable()
 export class LTIMiddleware implements NestMiddleware {
   ltiMiddleware: Express | undefined
 
-  constructor (private readonly configService: ConfigService) {}
-
-  public async setLTIMiddleware (): Promise<Express> {
-    const dbConfig = this.configService.get('db') as DatabaseConfig
-    const ltiConfig = this.configService.get('lti') as LTIConfig
-
-    const ltiMiddleware = await setUpLTI(dbConfig, ltiConfig)
-    this.ltiMiddleware = ltiMiddleware
-    return ltiMiddleware
+  constructor (private readonly ltiService: LTIService) {
+    this.ltiService.setUpLTI()
+      .then(app => {
+        this.ltiMiddleware = app
+        logger.info('ltijs was successfully set up.')
+      })
+      .catch(e => {
+        this.ltiMiddleware = undefined
+        throw new Error(`An error occurred while setting up ltijs: ${String(e)}`)
+      })
   }
 
   async use (req: Request, res: Response, next: NextFunction) {
-    let ltiMiddleware
-    if (this.ltiMiddleware === undefined) {
-      ltiMiddleware = await this.setLTIMiddleware()
-    } else {
-      ltiMiddleware = this.ltiMiddleware
-    }
-    ltiMiddleware(req, res, next)
+    if (this.ltiMiddleware !== undefined) this.ltiMiddleware(req, res, next)
   }
 }
--- ccm_web/server/src/lti/lti.module.ts
+++ ccm_web/server/src/lti/lti.module.ts
@@ -2,9 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { LTIMiddleware } from './lti.middleware'
+import { LTIService } from './lti.service'
 
 @Module({
-  imports: [ConfigModule]
+  imports: [ConfigModule],
+  providers: [LTIService]
 })
 export class LtiModule implements NestModule {
   configure (consumer: MiddlewareConsumer) {
