2cb93e307ddfbd46e305af37ae8c734039a0adc5
Sam Sciolla <ssciolla@umich.edu>
2021-05-04 12:52:08 -0400

    Fix tests for AppController

--- ccm_web/server/src/app.controller.spec.ts
+++ ccm_web/server/src/app.controller.spec.ts
@@ -14,9 +14,19 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController)
   })
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+  describe('hello', () => {
+    it('should return data with a "Hooray" message', () => {
+      expect(appController.getHello()).toStrictEqual({
+        message: 'You successfully communicated with the backend server. Hooray!'
+      })
+    })
+  })
+
+  describe('globals', () => {
+    it('should return globals data', () => {
+      expect(appController.getGlobals()).toStrictEqual({
+        environment: process.env.NODE_ENV
+      })
     })
   })
 })
