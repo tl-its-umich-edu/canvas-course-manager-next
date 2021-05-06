78454a00e86fd6b18c26d32b937426ed290aa629
Sam Sciolla <ssciolla@umich.edu>
2021-05-06 13:41:15 -0400

    Tidy up commands; remove rimraf; use modified tsconfig.build.json for prod

--- ccm_web/.eslintrc.js
+++ ccm_web/.eslintrc.js
@@ -35,11 +35,5 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
+  ignorePatterns: ['.eslintrc.js']
 }
