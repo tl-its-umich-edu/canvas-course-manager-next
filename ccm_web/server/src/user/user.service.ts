3fd81c030ebafd0c84db9eac56177d7ce863ba38
Pushyami Gundala <pushyamig@users.noreply.github.com>
2021-05-20 08:24:11 -0400

    Update ccm_web/server/src/user/user.service.ts
    Co-authored-by: Sam Sciolla <35741256+ssciolla@users.noreply.github.com>

--- ccm_web/server/src/user/user.service.ts
+++ ccm_web/server/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { CreateUserDto } from './dto/create-user.dto'
 import { User } from './user.model'
+
 @Injectable()
 export class UserService {
   constructor (
