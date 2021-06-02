f325340edd64423bf1da58308dd5aa945ff906d7
Sam Sciolla <ssciolla@umich.edu>
2021-06-02 15:20:33 -0400

    Remove changes to client Globals interface

--- ccm_web/client/src/models/models.ts
+++ ccm_web/client/src/models/models.ts
@@ -4,10 +4,6 @@ Interfaces for common objects and entities (e.g. Globals, Course, Section, etc.)
 
 export interface Globals {
   environment: 'production' | 'development'
-  canvasAuthURL: string
-  user?: {
-    hasAuthorized: boolean
-  }
 }
 
 export interface HelloMessageData {
