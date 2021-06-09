08183ae8d608340e8fdb06e67e08126de37014a3
Sam Sciolla <ssciolla@umich.edu>
2021-06-09 17:16:04 -0400

    Add validation for body, newName

--- ccm_web/server/src/api/dtos/api.course.name.dto.ts
+++ ccm_web/server/src/api/dtos/api.course.name.dto.ts
@@ -1,7 +1,10 @@
+import { IsNotEmpty, MaxLength } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 
 export class CourseNameDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @MaxLength(255)
   newName: string
 
   constructor (newName: string) {
