33a6e803d2101f51ff530cd195c6d31f83184724
pushyami <pushyami@umich.edu>
2021-06-10 11:40:38 -0400

    Expanding the global object in client

--- ccm_web/client/src/models/models.ts
+++ ccm_web/client/src/models/models.ts
@@ -4,6 +4,9 @@ Interfaces for common objects and entities (e.g. Globals, Course, Section, etc.)
 
 export interface Globals {
   environment: 'production' | 'development'
+  userLoginId: string
+  courseId: number
+  roles: string[]
 }
 
 export interface HelloMessageData {
