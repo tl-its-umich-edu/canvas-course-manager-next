86157c4a08e64f6d7efb248087cf4ed8ea8f178f
Sam Sciolla <ssciolla@umich.edu>
2021-07-26 14:45:46 -0400

    Tidy up comment for express localTypes

--- ccm_web/server/localTypes/express/index.d.ts
+++ ccm_web/server/localTypes/express/index.d.ts
@@ -1,5 +1,7 @@
-// TO DO: Is this the right way to do this?
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/23976#issuecomment-546404481
+/*
+Reference(s):
+- https://github.com/DefinitelyTyped/DefinitelyTyped/issues/23976#issuecomment-546404481
+*/
 
 type UserModel = import('../../src/user/user.model').User
 
