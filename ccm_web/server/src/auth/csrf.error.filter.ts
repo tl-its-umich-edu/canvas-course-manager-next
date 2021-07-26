7c0323a8bdee56884fb7b74b9120cae6b198f8da
Sam Sciolla <ssciolla@umich.edu>
2021-07-26 17:11:11 -0400

    Make CSRFErrorFilter inherit BaseExceptionFilter so other exceptions are appropriately handled

--- ccm_web/server/src/auth/csrf.error.filter.ts
+++ ccm_web/server/src/auth/csrf.error.filter.ts
@@ -1,16 +1,18 @@
 import { Response } from 'express'
-import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common'
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common'
+import { BaseExceptionFilter } from '@nestjs/core'
 
 import { MaybeCSRFError } from './auth.interfaces'
 
 /*
 Resource(s):
+- https://docs.nestjs.com/exception-filters#inheritance
 - https://github.com/nestjs/docs.nestjs.com/issues/215#issuecomment-689714257
 - https://github.com/expressjs/csurf#custom-error-handling
 */
 
 @Catch()
-export class CSRFErrorFilter<T extends MaybeCSRFError> implements ExceptionFilter {
+export class CSRFErrorFilter<T extends MaybeCSRFError> extends BaseExceptionFilter {
   catch (exception: T, host: ArgumentsHost): void {
     const res = host.switchToHttp().getResponse<Response>()
     if (exception.code === 'EBADCSRFTOKEN') {
@@ -18,6 +20,8 @@ export class CSRFErrorFilter<T extends MaybeCSRFError> implements ExceptionFilte
         statusCode: HttpStatus.FORBIDDEN,
         message: 'CSRF token was missing or invalid.'
       })
+    } else {
+      super.catch(exception, host)
     }
   }
 }
