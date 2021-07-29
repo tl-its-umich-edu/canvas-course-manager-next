c1b9d964c7a0ba0da6b8bcadabd5fc3c9567e9e5
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-07-29 16:46:11 -0400

    #108 - working section code into enrollment code

--- ccm_web/server/src/api/api.enroll.section.users.handler.ts
+++ ccm_web/server/src/api/api.enroll.section.users.handler.ts
@@ -4,66 +4,66 @@ import { APIErrorData, isAPIErrorData } from './api.interfaces'
 import { handleAPIError } from './api.utils'
 
 import baseLogger from '../logger'
-import { CanvasCourseSection } from '../canvas/canvas.interfaces'
+import { CanvasEnrollment } from '../canvas/canvas.interfaces'
 
 const logger = baseLogger.child({ filePath: __filename })
 
 export class EnrollSectionUsersApiHandler {
   requestor: CanvasRequestor
-  sections: string[]
-  courseId: number
+  users: string[]
+  sectionId: number
 
-  constructor (requestor: CanvasRequestor, sections: string[], courseId: number) {
+  constructor (requestor: CanvasRequestor, users: string[], sectionId: number) {
     this.requestor = requestor
-    this.sections = sections
-    this.courseId = courseId
+    this.users = users
+    this.sectionId = sectionId
   }
 
-  async createSection (sectionName: string): Promise<CanvasCourseSection | APIErrorData> {
-    try {
-      const endpoint = `courses/${this.courseId}/sections`
-      const method = 'POST'
-      const requestBody = { course_section: { name: sectionName } }
-      logger.debug(`Sending request to Canvas - Endpoint: ${endpoint}; Method: ${method}; Body: ${JSON.stringify(requestBody)}`)
-      const response = await this.requestor.requestUrl<CanvasCourseSection>(endpoint, method, requestBody)
-      const { id, name } = response.body
-      return { id, name }
-    } catch (error) {
-      const errResponse = handleAPIError(error, sectionName)
-      return { statusCode: errResponse.canvasStatusCode, errors: [errResponse] }
-    }
-  }
-
-  makeReturnResponseCreateSections (sectionsReturnRes: Array<APIErrorData | CanvasCourseSection>): CanvasCourseSection[] | APIErrorData {
-    const successes = []; const statusCodes = []; const errorsList = []
-    for (const section of sectionsReturnRes) {
-      if (isAPIErrorData(section)) {
-        const { statusCode, errors } = section
-        errorsList.push(...errors)
-        statusCodes.push(statusCode)
+  makeEnrollmentResponse (enrollmentResult: Array<APIErrorData | CanvasEnrollment>): CanvasEnrollment[] | APIErrorData {
+    const errors = [], statusCodes: Set<number> = new Set(), successes = []
+      for (const enrollment of enrollmentResult) {
+      if (isAPIErrorData(enrollment)) {
+        const { statusCode, error } = enrollment
+        errors.push(...error)
+        statusCodes.add(statusCode)
       } else {
-        successes.push(section)
+        successes.push(enrollment)
       }
     }
 
-    if (successes.length === this.sections.length) {
+    if (successes.length === this.users.length) {
       return successes
     } else {
-      const uniqueStatusCodes = [...new Set(statusCodes)]
       return {
-        statusCode: uniqueStatusCodes.length > 1 ? 502 : uniqueStatusCodes[0],
-        errors: errorsList
+        statusCode: statusCodes.size > 1 ? 502 : [...statusCodes][0],
+        error: errors
       }
     }
   }
 
-  async enrollUsers (): Promise<CanvasCourseSection[] | APIErrorData> {
+  async enrollUser (userLoginId: string): Promise<CanvasEnrollment | APIErrorData> {
+    try {
+      const endpoint = `sections/${this.sectionId}/enrollments`
+      const method = 'POST'
+      const body = { enrollment: { user_id: userLoginId } }
+      logger.debug(`Sending request to Canvas endpoint: ${endpoint}; method: ${method}; body: ${JSON.stringify(body)}`)
+      const response = await this.requestor.requestUrl<CanvasEnrollment>(endpoint, method, body)
+      const { id, course_id, course_section_id, user_id } = response.body
+      return { id, course_id, course_section_id, user_id }
+    } catch (error) {
+      const errResponse = handleAPIError(error, userLoginId)
+      return { statusCode: errResponse.canvasStatusCode, error: [errResponse] }
+    }
+  }
+
+
+  async enrollUsers (): Promise<CanvasEnrollment[] | APIErrorData> {
     const NS_PER_SEC = BigInt(1e9)
     const start = process.hrtime.bigint()
-    const apiPromises = this.sections.map(async (section) => await this.createSection(section))
-    const sectionsOrErrorDataObjs = await Promise.all(apiPromises)
+    const apiPromises = this.users.map(async (userLoginId) => await this.enrollUser(userLoginId))
+    const enrollmentResponses = await Promise.all(apiPromises)
     const end = process.hrtime.bigint();
-    logger.debug(`Time taken to create ${this.sections.length} sections: ${(end - start) / NS_PER_SEC} seconds`)
-    return this.makeReturnResponseCreateSections(sectionsOrErrorDataObjs)
+    logger.debug(`Time elapsed to enroll (${this.users.length}) users: (${(end - start) / NS_PER_SEC}) seconds`)
+    return this.makeEnrollmentResponse(enrollmentResponses)
   }
 }
