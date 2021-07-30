8cc4f4f580dccf1995939fb77e7d3fbe1f7e55b0
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-07-30 16:13:39 -0400

    #108 - formatting; better symbol names

--- ccm_web/server/src/api/api.enroll.section.users.handler.ts
+++ ccm_web/server/src/api/api.enroll.section.users.handler.ts
@@ -49,24 +49,36 @@ export class EnrollSectionUsersApiHandler {
       const endpoint = `sections/${this.sectionId}/enrollments`
       const method = 'POST'
       const body = { enrollment: { user_id: userLoginId } }
-      logger.debug(`Sending request to Canvas endpoint: ${endpoint}; method: ${method}; body: ${JSON.stringify(body)}`)
+      logger.debug(`Sending request to Canvas endpoint: "${endpoint}"; method: "${method}"; body: "${JSON.stringify(body)}"`)
       const response = await this.requestor.requestUrl<CanvasEnrollment>(endpoint, method, body)
-      const { id, course_id, course_section_id, user_id } = response.body
-      return { id, course_id, course_section_id, user_id }
+      const {
+        id,
+        course_id,
+        course_section_id,
+        user_id
+      } = response.body
+      return {
+        id,
+        course_id,
+        course_section_id,
+        user_id
+      }
     } catch (error) {
-      const errResponse = handleAPIError(error, userLoginId)
-      return { statusCode: errResponse.canvasStatusCode, error: [errResponse] }
+      const errorResponse = handleAPIError(error, userLoginId)
+      return {
+        statusCode: errorResponse.canvasStatusCode,
+        errors: [errorResponse]
+      }
     }
   }
-
 
   async enrollUsers (): Promise<CanvasEnrollment[] | APIErrorData> {
     const NS_PER_SEC = BigInt(1e9)
     const start = process.hrtime.bigint()
     const apiPromises = this.users.map(async (userLoginId) => await this.enrollUser(userLoginId))
     const enrollmentResponses = await Promise.all(apiPromises)
-    const end = process.hrtime.bigint();
+    const end = process.hrtime.bigint()
     logger.debug(`Time elapsed to enroll (${this.users.length}) users: (${(end - start) / NS_PER_SEC}) seconds`)
-    return this.makeEnrollmentResponse(enrollmentResponses)
+    return this.makeResponse(enrollmentResponses)
   }
 }
