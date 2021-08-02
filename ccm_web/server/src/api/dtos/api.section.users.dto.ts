20799748a704aaa99169be32917689b80dc0424a
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-08-02 11:33:33 -0400

    #108 - add validation for users with roles

--- ccm_web/server/src/api/dtos/api.section.users.dto.ts
+++ ccm_web/server/src/api/dtos/api.section.users.dto.ts
@@ -1,13 +1,26 @@
 import { IsNotEmpty, MaxLength } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 
+export class SectionUserDto {
+  @IsNotEmpty()
+  user: string
+
+  @IsNotEmpty()
+  role: string
+
+  constructor (user: string, role: string) {
+    this.user = user
+    this.role = role
+  }
+}
+
 export class SectionUsersDto {
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: [SectionUserDto] })
   @IsNotEmpty({ each: true })
   @MaxLength(400, { each: true })
-  users: string[]
+  users: SectionUserDto[]
 
-  constructor (users: string[]) {
+  constructor (users: SectionUserDto[]) {
     this.users = users
   }
 }
