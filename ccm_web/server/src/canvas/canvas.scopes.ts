1c72e4bf5e49a0c8e0965c8669c9c983dd730d77
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-08-03 10:41:50 -0400

    #108 - added scope required for enrolling users

--- ccm_web/server/src/canvas/canvas.scopes.ts
+++ ccm_web/server/src/canvas/canvas.scopes.ts
@@ -11,7 +11,9 @@ const privilegeLevelOneScopes = [
   'url:PUT|/api/v1/courses/:id',
   // Sections
   'url:GET|/api/v1/courses/:course_id/sections',
-  'url:POST|/api/v1/courses/:course_id/sections'
+  'url:POST|/api/v1/courses/:course_id/sections',
+  // Enrollments
+  'url:POST|/api/v1/sections/:section_id/enrollments'
 ]
 
 export { privilegeLevelOneScopes }
