4edd6457a3c710918d6baea6a161c78b3531a7dd
pushyami <pushyami@umich.edu>
2021-09-13 11:09:12 -0400

    #185 SC should see all the features

--- ccm_web/client/src/models/feature.ts
+++ ccm_web/client/src/models/feature.ts
@@ -13,7 +13,7 @@ const mergeSectionProps: FeatureDataProps = {
   title: 'Merge Sections',
   description: 'Combine sections into one Canvas site for easier management',
   ordinality: 1,
-  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin']]
+  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum['Support Consultant']]
 }
 
 const canvasGradebookFormatterProps: FeatureDataProps = {
@@ -21,7 +21,7 @@ const canvasGradebookFormatterProps: FeatureDataProps = {
   title: 'Canvas Gradebook Formatter',
   description: 'Formats the exported Canvas Gradebook CSV file for uploading into Faculty Center\'s Grade Roster.',
   ordinality: 2,
-  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin']]
+  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum['Support Consultant']]
 }
 
 const ExternalToolsGradebookFormatterProps: FeatureDataProps = {
@@ -29,7 +29,7 @@ const ExternalToolsGradebookFormatterProps: FeatureDataProps = {
   title: 'External Tools Gradebook Formatter',
   description: 'Formats a CSV file exported from an external tool for importing grades into the Canvas Gradebook.',
   ordinality: 3,
-  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin']]
+  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum['Support Consultant']]
 }
 
 const createSectionsProps: FeatureDataProps = {
@@ -37,7 +37,7 @@ const createSectionsProps: FeatureDataProps = {
   title: 'Create Sections',
   description: 'Create sections through csv files into your own course',
   ordinality: 4,
-  roles: [RoleEnum['Subaccount admin'], RoleEnum['Account Admin']]
+  roles: [RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum['Support Consultant']]
 }
 
 const addUMUsersProps: FeatureDataProps = {
@@ -45,7 +45,7 @@ const addUMUsersProps: FeatureDataProps = {
   title: 'Add UM Users',
   description: 'Add UM users to your available sections',
   ordinality: 5,
-  roles: [RoleEnum['Subaccount admin'], RoleEnum['Account Admin']]
+  roles: [RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum['Support Consultant']]
 }
 
 const addNonUMUsersProps: FeatureDataProps = {
@@ -53,10 +53,10 @@ const addNonUMUsersProps: FeatureDataProps = {
   title: 'Add Non-UM Users',
   description: 'Enroll non-UM users to your available sections',
   ordinality: 6,
-  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum.Assistant, RoleEnum['Tool installer'], RoleEnum.TA, RoleEnum.Designer]// ?
+  roles: [RoleEnum.Teacher, RoleEnum['Subaccount admin'], RoleEnum['Account Admin'], RoleEnum['Support Consultant'], RoleEnum.Assistant, RoleEnum['Tool installer'], RoleEnum.TA, RoleEnum.Designer]// ?
 }
 
-const courseRenameRoles: RoleEnum[] = [RoleEnum['Account Admin'], RoleEnum['Subaccount admin']]
+const courseRenameRoles: RoleEnum[] = [RoleEnum['Account Admin'], RoleEnum['Subaccount admin'], RoleEnum['Support Consultant']]
 
 export type { FeatureDataProps }
 export { mergeSectionProps, canvasGradebookFormatterProps, ExternalToolsGradebookFormatterProps, createSectionsProps, addUMUsersProps, addNonUMUsersProps, courseRenameRoles }
