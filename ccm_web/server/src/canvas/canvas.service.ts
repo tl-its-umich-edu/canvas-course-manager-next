72a4b26ab855b6f3895440b60589e927ff34e507
Mr. Lance E Sloan «UMich» <lsloan-github.com@umich.edu>
2021-10-01 11:46:21 -0400

    Revert "Revert "use got default retry statusCodes (iss. #194)" (#211)"
    This reverts commit e827735d565a069c67654ab4a04d9c5d69528543.

--- ccm_web/server/src/canvas/canvas.service.ts
+++ ccm_web/server/src/canvas/canvas.service.ts
@@ -3,7 +3,7 @@ import CanvasRequestor from '@kth/canvas-api'
 import { HttpService, HttpStatus, Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { InjectModel } from '@nestjs/sequelize'
-import { Options as GotOptions } from 'got'
+import got, { Options as GotOptions } from 'got'
 
 import { CanvasOAuthAPIError, CanvasTokenNotFoundError, InvalidTokenRefreshError } from './canvas.errors'
 import { TokenCodeResponseBody, TokenRefreshResponseBody } from './canvas.interfaces'
@@ -29,9 +29,7 @@ const requestorOptions: GotOptions = {
   retry: {
     limit: 2,
     methods: ['POST', 'GET', 'PUT', 'DELETE'],
-    // TODO: After got@12 upgrade, replace following list with something like…
-    // got.defaultInternals.retry.statusCodes.concat(403)
-    statusCodes: [403, 408, 413, 429, 500, 502, 503, 504, 521, 522, 524]
+    statusCodes: got.defaults.options.retry.statusCodes.concat([403])
   },
   hooks: {
     beforeRequest: [
