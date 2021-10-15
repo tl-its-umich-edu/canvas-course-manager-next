e481f7a4f1ebfbe9c8ed80ad8b58f054cdc1f7dd
Sam Sciolla <ssciolla@umich.edu>
2021-10-15 18:09:11 -0400

    Remove use of List/ListItem for now, modify spacing/padding in ErrorAlert

--- ccm_web/client/src/components/ErrorAlert.tsx
+++ ccm_web/client/src/components/ErrorAlert.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Button, Grid, List, ListItem, makeStyles, Paper, Typography } from '@material-ui/core'
+import { Button, Grid, makeStyles, Paper, Typography } from '@material-ui/core'
 import ErrorIcon from '@material-ui/icons/Error'
 
 const useStyles = makeStyles((theme) => ({
+  padding: {
+    paddingLeft: 10,
+    paddingRight: 10
+  },
   dialog: {
     textAlign: 'center',
-    maxWidth: '75%',
     margin: 'auto',
     marginTop: 30,
     marginBottom: 15,
-    paddingLeft: 10,
-    paddingRight: 10,
-    width: '75%',
     '& ol': {
-      margin: 'auto',
-      width: '75%'
+      margin: 'auto'
     },
     '& li': {
-      textAlign: 'left'
+      textAlign: 'left',
+      marginBottom: 10
     }
   },
   dialogIcon: {
@@ -36,7 +36,7 @@ export default function ErrorAlert (props: ErrorAlertProps): JSX.Element {
   const defaultMessage = <Typography>Something went wrong. Please try again later.</Typography>
   const preface = (
     <Typography gutterBottom>
-      Some errors occurred. If possible, correct them, and/or try again.
+      Some errors occurred (see below). If possible, correct them, and/or try again.
     </Typography>
   )
 
@@ -46,11 +46,11 @@ export default function ErrorAlert (props: ErrorAlertProps): JSX.Element {
     ? defaultMessage
     : messages.length === 1
       ? messages[0]
-      : <List>{messages.map((m, i) => <ListItem key={i}>{m}</ListItem>)}</List>
+      : <ol>{messages.map((m, i) => <li key={i}>{m}</li>)}</ol>
 
   return (
-    <Grid item xs={12} className={classes.dialog}>
-      <Paper role='alert'>
+    <Grid item xs={12} sm={9} md={6} className={`${classes.dialog} ${classes.padding}`}>
+      <Paper className={classes.padding} role='alert'>
         {icon !== undefined ? icon : <ErrorIcon className={classes.dialogIcon} fontSize='large' />}
         {Boolean(messages?.length) && preface}
         {messageBlock}
