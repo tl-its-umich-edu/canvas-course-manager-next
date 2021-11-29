b3e4540c1dbb08f69a0574d36169d81759b080ea
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-11-29 15:41:14 -0500

    #221 - invitation service based on Canvas one

---
+++ ccm_web/server/src/invitation/invitation.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Config } from '../config'
+import { HttpService } from '@nestjs/axios'
+import FormData from 'form-data'
+import { randomUUID } from 'crypto'
+import axios from 'axios'
+import { InvitationAPIError } from './invitation.errors'
+import { SectionUserDto } from '../api/dtos/api.section.users.dto'
+
+
+// Invitation service based on Cirrus
+
+@Injectable()
+export class InvitationService {
+  url: string
+  entityID: string
+  sponsorName: string
+  key: string
+  secret: string
+
+  constructor (private readonly configService: ConfigService<Config, true>) {
+    const invitationConfig = configService.get('invitation', { infer: true })
+    // const domain = configService.get('server.domain', { infer: true })
+    this.url = invitationConfig.apiURL
+    this.entityID = invitationConfig.apiEntityID
+    this.sponsorName = invitationConfig.apiSponsorName
+    this.key = invitationConfig.apiKey
+    this.secret = invitationConfig.apiSecret
+  }
+
+  async sendInvitations (users: SectionUserDto[]): Promise<string> {
+    const userEmails: string[] = users.map(user => user.loginId)
+    const emailAddressCSV = `emailAddress\n${userEmails.join('\n')}`
+
+    const data = new FormData()
+    data.append('cfile', emailAddressCSV, 'fake_file_name.csv')
+    data.append('spEntityId', this.entityID)
+    data.append('sponsorEppn', this.sponsorName)
+    data.append('clientRequestID', 'ccm-' + randomUUID())
+
+    let results: string = 'n/a'
+
+    axios({
+      method: 'POST',
+      url: this.url,
+      auth: {
+        username: this.key,
+        password: this.secret
+      },
+      headers: { ...data.getHeaders() },
+      data: data
+    })
+      .then((response) => {
+        const results: string = JSON.stringify(response.data)
+        console.log(results)
+      })
+      .catch((error) => {
+        console.log(error)
+        throw new InvitationAPIError()
+      })
+
+    return results
+  }
+}
