11cc5898a891f43b518d9900d4d783d129f42662
Pushyami Gundala <pushyamig@users.noreply.github.com>
2021-12-15 08:16:58 -0500

    Update ccm_web/client/src/pages/Home.tsx
    Co-authored-by: Sam Sciolla <35741256+ssciolla@users.noreply.github.com>

--- ccm_web/client/src/pages/Home.tsx
+++ ccm_web/client/src/pages/Home.tsx
@@ -57,7 +57,7 @@ function Home (props: HomeProps): JSX.Element {
     setCourseNameAsync,
     (course: CanvasCourseBase|undefined) => {
       props.setCourse(course)
-      enqueueSnackbar('Course name was saved! Please refresh the page to see the change in Canvas.', { variant: 'success' })
+      enqueueSnackbar('Course name was saved! Please refresh the page to have the change appear in Canvas.', { variant: 'success' })
     }
   )
 
