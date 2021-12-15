d825b6a7b0f7ec5c2e4fcc484ea37b4109f63a40
pushyami <pushyami@umich.edu>
2021-12-15 12:11:35 -0500

    #265 #75 creating theme and fixing the error icon color

--- ccm_web/client/src/index.tsx
+++ ccm_web/client/src/index.tsx
@@ -1,18 +1,22 @@
 import { SnackbarProvider } from 'notistack'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' 
+import { ThemeProvider } from '@material-ui/core'
 
+import ccmTheme from './theme'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
+   <ThemeProvider theme={ccmTheme}>
     <SnackbarProvider maxSnack={3}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </SnackbarProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ ccm_web/client/src/theme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from '@material-ui/core/styles'
+
+const ccmTheme = createTheme({
+  palette: {
+    error: {
+      main: '#E31C3D'
+    },
+    warning: {
+      main: '#E2CF2A'
+    },
+    success: {
+      main: '#306430'
+    },
+    info: {
+      main: '#3F648E'
+    }
+  }
+})
+
+export default ccmTheme
