df8fd41e0120796349e0f5820601b25277f26dc3
Sam Sciolla <ssciolla@umich.edu>
2021-12-20 18:05:55 -0500

    Change allowed enrollment role logic

--- ccm_web/client/src/models/canvas.ts
+++ ccm_web/client/src/models/canvas.ts
@@ -89,103 +89,50 @@ const clientToCanvasRoleMap: Record<ClientEnrollmentType, CanvasEnrollmentType> 
   designer: CanvasEnrollmentType.Designer
 }
 
-interface AddableRoleData {
-  addable: true
-  rank: number
-  canvasName: CanvasEnrollmentType
-  clientName: ClientEnrollmentType
-}
+const levelOneAddableRoles = [ClientEnrollmentType.Student]
+const levelTwoAddableRoles = [...levelOneAddableRoles, ClientEnrollmentType.Observer]
+const levelThreeAddableRoles = [
+  ...levelTwoAddableRoles, ClientEnrollmentType.TA, ClientEnrollmentType.Designer, ClientEnrollmentType.Teacher
+]
 
-interface NotAddableRoleData {
-  rank: number
-  addable: false
-}
+type RankedRoleData = Record<RoleEnum, number>
 
-type RoleData = AddableRoleData | NotAddableRoleData
-
-type CanvasRoleData = Record<RoleEnum, RoleData>
-
-export const AllCanvasRoleData: CanvasRoleData = {
-  StudentEnrollment: {
-    rank: 0,
-    addable: true,
-    canvasName: CanvasEnrollmentType.Student,
-    clientName: ClientEnrollmentType.Student
-  },
-  Assistant: {
-    rank: 1,
-    addable: false
-  },
-  Observer: {
-    rank: 2,
-    addable: true,
-    canvasName: CanvasEnrollmentType.Observer,
-    clientName: ClientEnrollmentType.Observer
-  },
-  TaEnrollment: {
-    rank: 3,
-    addable: true,
-    canvasName: CanvasEnrollmentType.Observer,
-    clientName: ClientEnrollmentType.TA
-  },
-  TeacherEnrollment: {
-    rank: 4,
-    addable: true,
-    canvasName: CanvasEnrollmentType.Teacher,
-    clientName: ClientEnrollmentType.Teacher
-  },
-  DesignerEnrollment: {
-    rank: 5,
-    addable: true,
-    canvasName: CanvasEnrollmentType.Designer,
-    clientName: ClientEnrollmentType.Designer
-  },
-  'Sub-Account Admin': {
-    rank: 6,
-    addable: false
-  },
-  'Account Admin': {
-    rank: 7,
-    addable: false
-  },
-  'Tool Installer (by ITS Approval only)': {
-    rank: -1,
-    addable: false
-  },
-  'Support Consultant': {
-    rank: -1,
-    addable: false
-  },
-  Librarian: {
-    rank: -1,
-    addable: false
-  },
-  Grader: {
-    rank: -1,
-    addable: false
-  }
+export const rankedRoleData: RankedRoleData = {
+  StudentEnrollment: 0,
+  Observer: 0,
+  Assistant: 1,
+  TaEnrollment: 2,
+  TeacherEnrollment: 3,
+  DesignerEnrollment: 3,
+  'Sub-Account Admin': 3,
+  'Account Admin': 3,
+  'Tool Installer (by ITS Approval only)': 0,
+  'Support Consultant': 0,
+  Librarian: 0,
+  Grader: 0
 } as const
 
 const getMostPrivilegedRole = (roles: RoleEnum[]): RoleEnum => {
   if (roles.length === 0) throw new Error('Roles array must contain one or more roles.')
   return roles.sort(
-    (a, b) => AllCanvasRoleData[a].rank > AllCanvasRoleData[b].rank ? -1 : 1
+    (a, b) => rankedRoleData[a] > rankedRoleData[b] ? -1 : 1
   )[0]
 }
 
 export const getRolesUserCanEnroll = (roles: RoleEnum[]): ClientEnrollmentType[] => {
   const mostPrivRole = getMostPrivilegedRole(roles)
-  const mostPrivRoleData = AllCanvasRoleData[mostPrivRole]
   console.log('Most privileged role: ' + mostPrivRole)
-  const rolesUserCanEnroll: ClientEnrollmentType[] = []
-  for (const role of Object.keys(AllCanvasRoleData)) {
-    const roleData = AllCanvasRoleData[role as RoleEnum]
-    if (roleData.addable && roleData.rank < mostPrivRoleData.rank) {
-      rolesUserCanEnroll.push(roleData.clientName)
-    }
+  const rank = rankedRoleData[mostPrivRole]
+  switch (rank) {
+    case 1:
+      return levelOneAddableRoles
+    case 2:
+      return levelTwoAddableRoles
+    case 3:
+      return levelThreeAddableRoles
+    default:
+      return []
   }
-  console.log('Roles user can add: ' + rolesUserCanEnroll.toString())
-  return rolesUserCanEnroll
 }
 
 export const isValidRole = (role: string): role is ClientEnrollmentType => {
