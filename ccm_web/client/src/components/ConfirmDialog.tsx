2e35205a76ff3db7455cc9f95d0d501627e48bd0
Sam Sciolla <ssciolla@umich.edu>
2022-01-04 17:58:25 -0500

    Remove unused import

--- ccm_web/client/src/components/ConfirmDialog.tsx
+++ ccm_web/client/src/components/ConfirmDialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Link, makeStyles, Paper, Typography } from '@material-ui/core'
+import { Button, makeStyles, Paper, Typography } from '@material-ui/core'
 import CloudDoneIcon from '@material-ui/icons/CloudDone'
 
 import { DownloadData } from '../models/models'
