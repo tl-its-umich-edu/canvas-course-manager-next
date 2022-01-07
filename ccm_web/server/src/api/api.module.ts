5894203058b67e96051ad8a97d35953707697e5d
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2022-01-07 16:36:44 -0500

    #220 - update Cirrus references

--- ccm_web/server/src/api/api.module.ts
+++ ccm_web/server/src/api/api.module.ts
@@ -5,12 +5,12 @@ import { APIController } from './api.controller'
 import { APIService } from './api.service'
 import { CanvasModule } from '../canvas/canvas.module'
 import { CanvasService } from '../canvas/canvas.service'
-import { InvitationModule } from '../invitation/invitation.module'
-import { InvitationService } from '../invitation/invitation.service'
+import { CirrusInvitationModule } from '../invitation/cirrus-invitation.module'
+import { CirrusInvitationService } from '../invitation/cirrus-invitation.service'
 
 @Module({
-  imports: [CanvasModule, ConfigModule, InvitationModule],
-  providers: [APIService, CanvasService, InvitationService],
+  imports: [CanvasModule, ConfigModule, CirrusInvitationModule],
+  providers: [APIService, CanvasService, CirrusInvitationService],
   controllers: [APIController]
 })
 export class APIModule {}
