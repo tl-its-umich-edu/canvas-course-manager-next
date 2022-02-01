7e964c33d87a7ad4d77562ad4e5a90544bd0302b
Sam Sciolla <ssciolla@umich.edu>
2022-02-01 13:54:06 -0500

    Make tab order sequential; add hover/ripple behavior using CardActionArea

--- ccm_web/client/src/components/FeatureCard.tsx
+++ ccm_web/client/src/components/FeatureCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { Card, CardContent, Grid, Typography } from '@material-ui/core'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import { Card, CardActionArea, CardContent, Grid, makeStyles, Typography } from '@material-ui/core'
+
 import { FeatureUIProps } from '../models/FeatureUIData'
 
 const useStyles = makeStyles((theme) => ({
@@ -24,27 +24,29 @@ function FeatureCard (props: FeatureUIProps): JSX.Element {
   const classes = useStyles()
 
   return (
-    <Link className={classes.cardLink} to={props.route} >
-      <Card className={`${classes.root}`} variant="outlined" tabIndex={props.data.ordinality}>
-        <CardContent>
-          <Grid container>
-            <Grid item xs={12}>
-              <div className={classes.centered}>
-                {props.icon}
-              </div>
-              <div className={classes.centered}>
-                <Typography className={classes.title} color="textPrimary" gutterBottom>
-                  {props.data.title}
-                </Typography>
-                <Typography variant="body2" component="p" color="textSecondary">
-                  {props.data.description}
-                </Typography>
-              </div>
+    <RouterLink className={classes.cardLink} to={props.route} tabIndex={-1}>
+      <Card variant='outlined'>
+        <CardActionArea>
+          <CardContent className={`${classes.root}`}>
+            <Grid container>
+              <Grid item xs={12}>
+                <div className={classes.centered}>
+                  {props.icon}
+                </div>
+                <div className={classes.centered}>
+                  <Typography className={classes.title} color='textPrimary' gutterBottom>
+                    {props.data.title}
+                  </Typography>
+                  <Typography variant='body2' component='p' color='textSecondary'>
+                    {props.data.description}
+                  </Typography>
+                </div>
+              </Grid>
             </Grid>
-          </Grid>
-        </CardContent>
+          </CardContent>
+        </CardActionArea>
       </Card>
-    </Link>
+    </RouterLink>
   )
 }
 
