0683848be709f1b5a6bc119144ea1cf206818bde
pushyami <pushyami@umich.edu>
2022-02-01 09:26:59 -0500

    Adding global underline link

--- ccm_web/client/src/components/canvasSettingsLink.tsx
+++ ccm_web/client/src/components/canvasSettingsLink.tsx
@@ -10,6 +10,6 @@ export default function CanvasSettingsLink (linkProps: CanvasSettingsLinkProps):
   const url = linkProps.url
   const msg = linkProps.msg == null ? 'Canvas Settings page' : linkProps.msg
   return (
-      <Link style={{ textDecoration: 'underline' }} href={url} target='_parent'>{msg}</Link>
+    <Link href={url} target='_parent'>{msg}</Link>
   )
 }
