7fcd73f743569dccd45572ee767440c60abb8935
pushyami <pushyami@umich.edu>
2022-02-08 10:24:20 -0500

    Custom role from config

--- ccm_web/server/src/api/api.service.ts
+++ ccm_web/server/src/api/api.service.ts
@@ -116,7 +116,7 @@ export class APIService {
   }
 
   async enrollSectionUsers (user: User, sectionId: number, sectionUsers: SectionUserDto[]): Promise<CanvasEnrollment[] | APIErrorData> {
-    
+    const customRoles = JSON.parse(this.configService.get('canvas.customRoles', { infer: true }))
     const requestor = await this.canvasService.createRequestorForUser(user, '/api/v1/')
     const sectionHandler = new SectionApiHandler(requestor, sectionId)
     return await sectionHandler.enrollUsers(sectionUsers)
