58340eb9de7bdcc2f67388ab3c7ded5039cf341d
Sam Sciolla <ssciolla@umich.edu>
2022-02-21 13:15:13 -0500

    Add Divider before footer; add development paper with label and icon

--- ccm_web/client/src/components/Layout.tsx
+++ ccm_web/client/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Grid, Link, makeStyles, Typography } from '@material-ui/core'
+import { Divider, Grid, Link, Paper, makeStyles, Typography } from '@material-ui/core'
+import BuildIcon from '@material-ui/icons/Build'
 
 import Breadcrumbs, { BreadcrumbsProps } from './Breadcrumbs'
 import ResponsiveHelper from './ResponsiveHelper'
@@ -16,6 +17,9 @@ const useStyles = makeStyles((theme) => ({
   spacing: {
     marginBottom: theme.spacing(2)
   },
+  devModePaper: {
+    padding: theme.spacing(1)
+  },
   swaggerLink: {
     display: 'block',
     clear: 'both'
@@ -31,14 +35,21 @@ export default function Layout (props: LayoutProps): JSX.Element {
   const classes = useStyles()
 
   const devBlock = props.devMode === true && (
-    <div className={classes.spacing}>
-    <div className={classes.swaggerLink}>
-      <Link href={`/swagger?csrfToken=${String(getCSRFToken())}`} target='_blank'>Swagger UI</Link>
+    <>
+    <div className={`${classes.swaggerLink} ${classes.spacing}`}>
+      <Paper variant='outlined' className={classes.devModePaper}>
+        <Typography component='span' variant='subtitle1'>
+          <BuildIcon fontSize='small' /> Development Mode:&nbsp;
+        </Typography>
+        <Typography component='span'>
+          <Link href={`/swagger?csrfToken=${String(getCSRFToken())}`} target='_blank'>Swagger UI</Link>
+        </Typography>
+      </Paper>
     </div>
     <div style={{ position: 'fixed', right: '25px', top: '25px', zIndex: 999 }}>
       <ResponsiveHelper />
     </div>
-    </div>
+    </>
   )
 
   return (
@@ -47,6 +58,7 @@ export default function Layout (props: LayoutProps): JSX.Element {
         {devBlock}
         <div className={classes.breadcrumbsContainer}><Breadcrumbs {...props} /></div>
         <div className={classes.spacing}>{props.children}</div>
+        <Divider className={classes.spacing} />
         <footer>
           <Typography>Copyright © 2022 The Regents of the University of Michigan</Typography>
         </footer>
