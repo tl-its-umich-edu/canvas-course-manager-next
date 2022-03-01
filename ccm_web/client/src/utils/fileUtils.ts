4306b2758513820546d7835ca573c5e4458e1956
pushyami <pushyami@umich.edu>
2022-03-01 10:21:41 -0500

    #358 file name change when downloading csv

--- ccm_web/client/src/utils/fileUtils.ts
+++ ccm_web/client/src/utils/fileUtils.ts
@@ -5,14 +5,18 @@ const prepDownloadDataString = (data: string): string => {
 }
 
 /*
-Takes a file name and replaces the string segment without a period
+Takes a file name and replaces the string segment(as optional) without a period
 just before the file name extension with that segment plus a new provided ending.
+the default string segment is '-formatted'
 Examples:
-  createOutputFileName('old-file-name.csv', '-puff') returns 'old-file-name-puff.csv'
-  createOutputFileName('old-file-name', '-puff') returns 'old-file-name-puff
-  createOutputFileName('old.file.name.csv', '-puff') returns 'old.file.name-puff.csv'
+  createOutputFileName('old-file-name.csv', '-new') returns 'old-file-name-new.csv'
+  createOutputFileName('old-file-name') returns 'old-file-name-formatted
+  createOutputFileName('old.file.name.csv', '-alt') returns 'old.file.name-alt.csv'
 */
-function createOutputFileName (oldFileName: string, newfileEnding: string): string {
+function createOutputFileName (oldFileName: string, newfileEnding?: string): string {
+  if (newfileEnding === undefined) {
+    newfileEnding = '-formatterd'
+  }
   const splitName = oldFileName.split('.')
   const filenameIndex = splitName.length >= 2 ? splitName.length - 2 : 0
   splitName[filenameIndex] = splitName[filenameIndex] + newfileEnding
