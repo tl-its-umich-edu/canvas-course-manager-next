4f6770bb42cd762fa4881b238b01ba8b9913f8fa
Sam Sciolla <ssciolla@umich.edu>
2022-03-03 14:16:56 -0500

    Move comment

--- ccm_web/client/src/models/models.ts
+++ ccm_web/client/src/models/models.ts
@@ -2,12 +2,12 @@
 Interfaces for common objects and entities (e.g. Globals, Course, Section, etc.)
 */
 
-// Type guard ensuring an unknown value is a non-null object and has all the specified keys
-// https://stackoverflow.com/a/45390578
 function isRecord (x: unknown): x is Record<string | number | symbol, unknown> {
   return (typeof x === 'object' && x !== null)
 }
 
+// Type guard ensuring an unknown value is a non-null object and has all the specified keys
+// https://stackoverflow.com/a/45390578
 export function hasKeys<K extends string> (x: unknown, keys: K[]): x is Record<K, unknown> {
   if (!isRecord(x)) return false
   return keys.every(k => k in x)
