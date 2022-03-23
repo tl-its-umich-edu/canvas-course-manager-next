cea7863a3ee44077a6dee63b9eb1f53cb6cec3b4
pushyami <pushyami@umich.edu>
2022-03-23 09:55:35 -0400

    Removed the extra ping

--- ccm_web/server/src/health/health.module.ts
+++ ccm_web/server/src/health/health.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
 import { TerminusModule } from '@nestjs/terminus'
 
 import { HealthController } from './health.controller'
 
 @Module({
   controllers: [HealthController],
-  imports: [ConfigModule, TerminusModule]
+  imports: [TerminusModule]
 })
 export class HealthModule {}
