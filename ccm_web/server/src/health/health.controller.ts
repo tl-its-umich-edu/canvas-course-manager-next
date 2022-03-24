253733e97c7bd9cccfef7d68bd4c3449af890e48
Pushyami Gundala <pushyamig@users.noreply.github.com>
2022-03-24 11:21:36 -0400

    Removed the extra ping (#366)

--- ccm_web/server/src/health/health.controller.ts
+++ ccm_web/server/src/health/health.controller.ts
@@ -1,26 +1,19 @@
 import { Controller, Get } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import {
-  HealthCheck, HealthCheckResult, HealthCheckService, HttpHealthIndicator, SequelizeHealthIndicator
+  HealthCheck, HealthCheckResult, HealthCheckService, SequelizeHealthIndicator
 } from '@nestjs/terminus'
-
-import { Config } from '../config'
 
 @Controller('health')
 export class HealthController {
   constructor (
-    private readonly configService: ConfigService<Config, true>,
     private readonly health: HealthCheckService,
-    private readonly http: HttpHealthIndicator,
     private readonly db: SequelizeHealthIndicator
   ) {}
 
   @Get()
   @HealthCheck()
   async check (): Promise<HealthCheckResult> {
-    const appDomain = this.configService.get('server.domain', { infer: true })
     return await this.health.check([
-      async () => await this.http.pingCheck('root', `https://${appDomain}`),
       async () => await this.db.pingCheck('database', { timeout: 3000 })
     ])
   }
