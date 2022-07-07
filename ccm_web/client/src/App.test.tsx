a9d863f7b1443fe19d0e104eed2f71c24ffdbb39
Sam Sciolla <ssciolla@umich.edu>
2022-07-07 11:33:37 -0400

    Remove unused React testing and libraries

--- ccm_web/client/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-
-// test('renders something', () => {
-//   render(<App />)
-//   const linkElement = screen.getByText(/something/i)
-//   expect(linkElement).toBeInTheDocument()
-// })
