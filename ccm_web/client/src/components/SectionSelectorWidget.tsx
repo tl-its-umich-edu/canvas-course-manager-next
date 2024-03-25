1e6c48585e658baefc402f436702c228ddbc4a16
Jaydon Krooss <jkrooss@umich.edu>
2024-03-25 14:56:57 -0400

    fix merge sections disabled selector, unmerging bug (408)

--- ccm_web/client/src/components/SectionSelectorWidget.tsx
+++ ccm_web/client/src/components/SectionSelectorWidget.tsx
@@ -15,6 +15,7 @@ import {
   InputLabel,
   List,
   ListItem,
+  ListItemButton,
   ListItemText,
   Menu,
   MenuItem,
@@ -65,7 +66,7 @@ const Root = styled('div')((
   [`& .${classes.listContainer}`]: {
     overflow: 'auto',
     marginBottom: '5px',
-    '&& .Mui-disabled': {
+    '& .Mui-disabled': {
       opacity: 1,
       '& > .MuiListItemAvatar-root': {
         opacity: theme.palette.action.disabledOpacity
@@ -85,7 +86,12 @@ const Root = styled('div')((
 
   [`& .${classes.listItemRoot}`]: {
     paddingTop: '0px',
-    paddingBottom: '0px'
+    paddingBottom: '0px',
+    paddingRight: '0px',
+    display: 'block',
+    '& > .MuiListItemSecondaryAction-root': {
+      position: 'relative',
+    },
   },
 
   [`& .${classes.listButton}`]: {
@@ -419,7 +425,6 @@ function SectionSelectorWidget (props: ISectionSelectorWidgetProps): JSX.Element
                 </Typography>
               )
             }
-            {unmergeButton(section)}
             <span style={props.showCourseName === true ? { float: 'right' } : undefined}>
               {`${section.total_students ?? '?'} students`}
             </span>
@@ -594,7 +599,7 @@ function SectionSelectorWidget (props: ISectionSelectorWidgetProps): JSX.Element
                 divider
                 disableGutters
                 classes={{ root: classes.listItemRoot }}
-                selected={isSelected}
+                secondaryAction={unmergeButton(section)}
                 className={(section.locked !== true && props.highlightUnlocked === true) ? classes.highlighted : undefined}
               >
                 <ButtonBase
