572d6b6390bad1b210259fbf8722099aa60a9e52
Jaydon Krooss <jkrooss@umich.edu>
2025-05-07 12:45:55 -0400

    fix unit tests to mock get_enrollments

--- backend/tests/test_section_enrollments_api_handler.py
+++ backend/tests/test_section_enrollments_api_handler.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from django.test import RequestFactory
 from django.urls import reverse
 from rest_framework.test import APITestCase
@@ -21,89 +21,88 @@ class CanvasSectionEnrollmentsAPIHandlerTests(APITestCase):
         self.request_factory = RequestFactory()
     
     # Mock Section Enrollment API handler view for testing
-    def get_mocked_view(self, enrollment_data=None, canvasException=None):
+    def get_mocked_view(self, mock_get_enrollments, enrollment_data=[], canvasException=None):
         mock_canvas = MagicMock()
         mock_manager = MagicMock(spec=CanvasCredentialManager)
         mock_canvas_error_handler = MagicMock(spec=CanvasErrorHandler)
 
         if canvasException:
-            http_api_error = HTTPAPIError(str(self.section_ids[0]), canvasException)
-            error_obj = CanvasErrorHandler()
-            error_obj.handle_canvas_api_exceptions([http_api_error])
-            mock_failing_section = MagicMock(spec=Section)
-            mock_failing_section.get_enrollments.side_effect = canvasException
-            mock_canvas.get_section.side_effect = [mock_failing_section] 
-            mock_canvas_error_handler.handle_canvas_api_exceptions.return_value = error_obj
+            def side_effect(*args, **kwargs):
+                raise canvasException
+            mock_get_enrollments.side_effect = side_effect
         else:
-            mock_sections = []
-            for section_id, enrollments in zip(self.section_ids, enrollment_data or []):
-                mock_section = MagicMock(spec=Section)
-                mock_section.get_enrollments.return_value = [
-                    Enrollment(mock_canvas._Canvas__requester, data) for data in enrollments
-                ]
-                mock_sections.append(mock_section)
-            mock_canvas.get_section.side_effect = mock_sections
+            section_id_to_enrollments = {}
+            for section_id in self.section_ids: # hacky, only 3 sections & assuming section IDs start from 1
+                matched_enrollments = enrollment_data[section_id - 1] 
+                enrollments = [Enrollment(mock_canvas._Canvas__requester, data) for data in matched_enrollments]
+                mock_paginated = MagicMock(spec=PaginatedList)
+                mock_paginated.__iter__.return_value = iter(enrollments)
+                section_id_to_enrollments[str(section_id)] = mock_paginated
+
+            mock_get_enrollments.side_effect = [section_id_to_enrollments[str(id)] for id in self.section_ids]
+
         mock_manager.get_canvasapi_instance.return_value = mock_canvas
         return CanvasSectionEnrollmentsAPIHandler(credential_manager=mock_manager)
     
-    def test_get_section_enrollments_success(self):
+    @patch('canvasapi.section.Section.get_enrollments')
+    def test_get_section_enrollments_success(self, mock_get_enrollments):
         request = self.request_factory.get(self.url)
         request.user = self.user
         request.query_params = {'section_ids': ','.join(map(str, self.section_ids))}
 
         test_enrollments = [
-        [
-            {
-                'id': 1,
-                'sis_user_id': '12345',
-                'course_section_id': self.section_ids[0],
-                'user': {
+            [
+                {
                     'id': 1,
-                    'name': 'Test User 1',
                     'sis_user_id': '12345',
-                    'login_id': 'test_student_1'
-                }
-            },
-            {
-                'id': 2,
-                'sis_user_id': '67890',
-                'course_section_id': self.section_ids[0],
-                'user': {
+                    'course_section_id': self.section_ids[0],
+                    'user': {
+                        'id': 1,
+                        'name': 'Test User 1',
+                        'sis_user_id': '12345',
+                        'login_id': 'test_student_1'
+                    }
+                },
+                {
                     'id': 2,
-                    'name': 'Test User 2',
                     'sis_user_id': '67890',
-                    'login_id': 'test_student_2'
+                    'course_section_id': self.section_ids[0],
+                    'user': {
+                        'id': 2,
+                        'name': 'Test User 2',
+                        'sis_user_id': '67890',
+                        'login_id': 'test_student_2'
+                    }
                 }
-            }
-        ],
-        [
-            {
-                'id': 3,
-                'sis_user_id': '54321',
-                'course_section_id': self.section_ids[1],
-                'user': {
+            ],
+            [
+                {
                     'id': 3,
-                    'name': 'Test User 3',
                     'sis_user_id': '54321',
-                    'login_id': 'test_student_3'
+                    'course_section_id': self.section_ids[1],
+                    'user': {
+                        'id': 3,
+                        'name': 'Test User 3',
+                        'sis_user_id': '54321',
+                        'login_id': 'test_student_3'
+                    }
                 }
-            }
-        ],
-        [
-            {
-                'id': 3,
-                'sis_user_id': '67890',
-                'course_section_id': self.section_ids[2],
-                'user': {
-                    'id': 2,
-                    'name': 'Test User 2',
+            ],
+            [
+                {
+                    'id': 3,
                     'sis_user_id': '67890',
-                    'login_id': 'test_student_2'
+                    'course_section_id': self.section_ids[2],
+                    'user': {
+                        'id': 2,
+                        'name': 'Test User 2',
+                        'sis_user_id': '67890',
+                        'login_id': 'test_student_2'
+                    }
                 }
-            }
+            ]
         ]
-    ]
-        view = self.get_mocked_view(enrollment_data=test_enrollments)
+        view = self.get_mocked_view(mock_get_enrollments, enrollment_data=test_enrollments)
         response = view.get(request)
         # Assert the response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -112,24 +111,26 @@ class CanvasSectionEnrollmentsAPIHandlerTests(APITestCase):
         self.assertIn('test_student_2', response.data)
         self.assertIn('test_student_3', response.data)
     
-    def test_get_section_enrollments_empty(self):
+    @patch('canvasapi.section.Section.get_enrollments')
+    def test_get_section_enrollments_empty(self, mock_get_enrollments):
         request = self.request_factory.get(self.url)
         request.user = self.user
         request.query_params = {'section_ids': ','.join(map(str, self.section_ids))}
 
         empty_enrollments = [[],[],[]]
-        view = self.get_mocked_view(enrollment_data=empty_enrollments)
+        view = self.get_mocked_view(mock_get_enrollments, enrollment_data=empty_enrollments)
         response = view.get(request)
         # Assert the response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, [])
 
-    def test_get_section_enrollments_exception(self):
+    @patch('canvasapi.section.Section.get_enrollments')
+    def test_get_section_enrollments_exception(self, mock_get_enrollments):
         request = self.request_factory.get(self.url)
         request.user = self.user
         request.query_params = {'section_ids': str(self.section_ids[0])}
 
-        view = self.get_mocked_view(canvasException=CanvasException('Canvas API error getting section enrollments'))
+        view = self.get_mocked_view(mock_get_enrollments, canvasException=CanvasException('Canvas API error getting section enrollments'))
         response = view.get(request)
         # Assert the response
         expected_dict = {
