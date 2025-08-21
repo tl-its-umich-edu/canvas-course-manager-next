decad7e726699b5bae2908bfd2acf6cff2d114ce
Jaydon Krooss <jkrooss@umich.edu>
2025-08-21 16:56:49 -0400

    pr suggestions - add types, use timeit decorator

--- backend/ccm/canvas_api/instructor_sections_api_handler.py
+++ backend/ccm/canvas_api/instructor_sections_api_handler.py
@@ -8,10 +8,11 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 
 from canvasapi.exceptions import CanvasException
+from canvasapi.course import Course
 from backend.ccm.canvas_api.canvas_credential_manager import CanvasCredentialManager
 from backend.ccm.canvas_api.canvasapi_serializer import CanvasObjectROSerializer
-from backend.ccm.canvas_api.course_section_api_handler import CanvasCourseSectionAPIHandler
 from backend.ccm.canvas_api.exceptions import CanvasErrorHandler, HTTPAPIError
+from backend.ccm.utils import timeit
 
 logger = logging.getLogger(__name__)
 
@@ -34,8 +35,8 @@ class CanvasInstructorSectionsAPIHandler(LoggingMixin, APIView):
         term_id = request.query_params.get('term_id')
         if not term_id:
             return Response({"error": "Term ID is required as a parameter"}, status=HTTPStatus.BAD_REQUEST)
+        canvas_api = self.credential_manager.get_canvasapi_instance(request)
         try:
-            canvas_api = self.credential_manager.get_canvasapi_instance(request)
             filtered_courses, course_instance_map = self._get_filtered_teacher_courses(canvas_api, term_id)
             response_data = self._attach_sections_to_courses(filtered_courses, course_instance_map)
             return Response(response_data, status=HTTPStatus.OK)
@@ -44,22 +45,23 @@ class CanvasInstructorSectionsAPIHandler(LoggingMixin, APIView):
             logger.error(f"Error retrieving instructor sections for user id {request.user.id}")
             return Response(self.canvas_error.to_dict(), status=self.canvas_error.to_dict().get('statusCode'))
 
-    def _get_filtered_teacher_courses(self, canvas_api: Canvas, term_id: str):
+    @timeit
+    def _get_filtered_teacher_courses(self, canvas_api: Canvas, term_id: str) -> tuple[list[dict], dict[int, Course]]:
         """Fetch and filter teacher courses by term_id and a map of the course instances. Returns (filtered_courses, course_instance_map)."""
         logger.info(f"Retrieving instructor courses for term_id: {term_id}")
         try:
-            courses = canvas_api.get_courses(enrollment_type='teacher')
-            course_instance_map = {course.id: course for course in courses}
+            courses = list(canvas_api.get_courses(enrollment_type='teacher'))
+            course_instance_map: dict[int, Course] = {course.id: course for course in courses}
             serializer = CanvasObjectROSerializer(courses, allowed_fields=self.courses_allowed_fields, many=True)
-            filtered_courses = [course for course in serializer.data if course.get('enrollment_term_id') == int(term_id)]
+            filtered_courses: list[dict] = [course for course in serializer.data if course.get('enrollment_term_id') == int(term_id)]
             logger.info(f"Filtered to {len(filtered_courses)} courses for term_id {term_id}")
             return filtered_courses, course_instance_map
         except (CanvasException, Exception) as e:
             failed_input = f"term_id {term_id}"
             raise HTTPAPIError(failed_input, e)
 
-
-    def _attach_sections_to_courses(self, courses_data, course_instance_map):
+    @timeit
+    def _attach_sections_to_courses(self, courses_data:list[dict] , course_instance_map:dict[int, Course]) -> list[dict]:
         """Attach sections to each course in courses_data."""
         for course in courses_data:
             course_id = course.get('id')
--- backend/ccm/utils.py
+++ backend/ccm/utils.py
@@ -1,4 +1,6 @@
+from functools import wraps
 import os
+import time
 from typing import List, Optional
 from csp.constants import SELF
 
@@ -20,4 +22,18 @@ def parse_csp(csp_key: str, extra_csp_sources: Optional[List[str]] = None) -> Li
             return DEFAULT_CSP_VALUE + csp_value + extra_csp_sources
         else:
             return DEFAULT_CSP_VALUE + csp_value 
+
+def timeit(func):
+    """
+    Decorator to measure the execution time of a function.
+    """
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"Function '{func.__name__}{args} {kwargs}' took {total_time:.4f} seconds to execute.")
+        return result
+    return timeit_wrapper
 
