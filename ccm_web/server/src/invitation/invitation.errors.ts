2c6355ad2480a8d20fc197868146c3f8d98bdff0
Pushyami Gundala <pushyamig@users.noreply.github.com>
2026-06-16 11:30:23 -0400

    #605 remove NodeJs backend  Server folder (#670)

--- ccm_web/server/src/invitation/invitation.errors.ts
+++
@@ -1,9 +0,0 @@
-export class InvitationAPIError extends Error {
-  date: Date
-  public name = 'InvitationAPIError'
-
-  constructor (message: string) {
-    super(`An error occurred while sending invitations: ${message}`)
-    this.date = new Date()
-  }
-}
