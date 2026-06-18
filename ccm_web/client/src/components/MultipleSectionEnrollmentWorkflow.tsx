ccab836928b823413566427a90afc1f2d8e3a6d3
Zhen Qian <zqian@umich.edu>
2026-06-18 09:57:40 -0400

    issue_593 wrap the section title to 50 characters per line (#656)
    * issue_593 wrap the section title to 50 characters per line
    
    * issue_593 updated according to the review comment

--- ccm_web/client/src/components/MultipleSectionEnrollmentWorkflow.tsx
+++ ccm_web/client/src/components/MultipleSectionEnrollmentWorkflow.tsx
@@ -235,7 +235,7 @@ export default function MultipleSectionEnrollmentWorkflow (props: MultipleSectio
     const tableCaption = `Names and Canvas IDs for the course's ${props.sections.length} sections`
     const sectionIdsTable = (
       <TableContainer className={classes.sectionIdTable}>
-        <Table stickyHeader>
+        <Table stickyHeader sx={{ tableLayout: 'fixed' }}>
           <TableCaption text={tableCaption} />
           <TableHead>
             <TableRow>
@@ -247,8 +247,12 @@ export default function MultipleSectionEnrollmentWorkflow (props: MultipleSectio
             {
               props.sections.map((s, i) => (
                 <TableRow tabIndex={0} key={i}>
-                  <TableCell>{s.name}</TableCell>
-                  <TableCell>{s.id}</TableCell>
+                  <TableCell sx={{ wordBreak: 'break-word', width: '75%' }}>
+                    {s.name}
+                  </TableCell>
+                  <TableCell sx={{ whiteSpace: 'nowrap', width: '25%' }}>
+                    {s.id}
+                  </TableCell>
                 </TableRow>
               ))
             }
